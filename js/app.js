b4a9df0c1aab2ab31dd96a69fe1eb79655296cf0
NodirJuraqulov <njuraqulov2288@gmail.com>
2025-03-17 12:45:38 +0500

    Add Lesson3_Homework

---
+++ js/app.js
@@ -0,0 +1,29 @@
+
+// const countEl = document.querySelector(".count")
+// let offset = 60
+
+// function inc(){
+//     offset--
+//     countEl.innerHTML = offset
+// }
+// inc()
+
+// const interval = setInterval(()=>{
+//     inc()
+//     if(offset <= 0){
+//         clearInterval(interval)
+//     }
+// }, 1000)
+
+
+
+// function timeFormat() {
+    
+// }
+
+// console.log(timeFormat(50));
+// console.log(timeFormat(100));
+
+
+
+
